3001e0da321fad579ff90a24e12cd6b7c674b7e6
iwpnd <legionaerr@googlemail.com>
2018-06-03 14:29:21 +0200

    add geopy to geocode adresses

--- shoptekt.py
+++ shoptekt.py
@@ -2,8 +2,10 @@ from flask import Flask, flash, render_template, request
 from flask import url_for, make_response
 import requests as re
 import pandas as pd
+from geopy.geocoders import Nominatim
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 def request_data(lat,lon):
     """
@@ -13,7 +15,7 @@ def request_data(lat,lon):
     lat -- latitude in epsg4326
     lon -- longitude in epsg4326
     """
-    url = 'http://www.meinprospekt.de/service/Map/getBranchMarkers?categoryId=5%2C2%2C%2C3&lat={}&lng={}&limit=50&web=1&version=2.0'.format(lat,lon)
+    url = 'http://www.meinprospekt.de/service/Map/getBranchMarkers?categoryId=5%2C2%2C%2C3&lat={}&lng={}&limit=5&web=1&version=2.0'.format(lat,lon)
     return re.get(url).json()['data']
 
 
@@ -53,29 +55,63 @@ def format_response_df(df):
     return df
 
 
+def geocode_adress(adress):
+    """
+    take adress as input and geocode. 
+    return lat lon of adress
+
+    input: adress -- text
+    """
+
+    geocoder = Nominatim()
+    location = geocoder.geocode(adress)
+    return location
+    
+
+def make_csv_response(df):
+    resp = make_response(df.to_csv())
+    resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return resp
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    error=None
     if request.method == 'POST':
         # request form data on post
         lon = request.form['lon']
         lat = request.form['lat']
+        adress = request.form['adress']
 
-        # get request response data, put it into a pandas
-        # dataframe and format it as you wish
-        response = request_data(lat,lon)
-        df = create_df_from_response(response)
-        df = format_response_df(df)
+        if lon and lat:
+            response = request_data(lat, lon)
+            df = create_df_from_response(response)
+            df = format_response_df(df)
+            return make_csv_response(df)
+        elif adress:
+            location = geocode_adress(adress)
+            try:
+                response = request_data(location.latitude, location.longitude)
+                df = create_df_from_response(response)
+                df = format_response_df(df)
+                return make_csv_response(df)
+            except KeyError:
+                error='{} is not a valid adress'.format(adress)
+                return render_template('shopstekt.html', error=error)
+        elif lon and lat and adress:
+            response = request_data(lat, lon)
+            df = create_df_from_response(response)
+            df = format_response_df(df)
+            return make_csv_response(df)
 
-        # make_response object csv
-        resp = make_response(df.to_csv())
-        resp.headers["Content-Disposition"] = "attachment; filename=export.csv"
-        resp.headers["Content-Type"] = "text/csv"
-        return resp
+        if not lon and not lat and not adress:
+            error = 'either provide lat/lon or an adress'
+            return render_template('shopstekt.html', error=error)
 
      # Render file input form
-    return render_template('shopstekt.html')
+    return render_template('shopstekt.html', error=error)
 
  
 # We only need this for local development.
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
